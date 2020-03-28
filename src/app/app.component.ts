886a99cc8003756175e4688290671b05e6fc247f
Jacob Tan <zhangen69@gmail.com>
2020-03-29 05:50:54 +0800

    preview image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { fromEvent, from } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-image-processing-app';
+  url = null;
+  imagePath = null;
+  image = null;
+  @ViewChild('imageOutput') imageOutput: ElementRef;
+  observer;
+
+  constructor(private eleRef: ElementRef, private http: HttpClient) {
+  }
+
+  onFileChanged(event) {
+    if (event.target.files && event.target.files.length > 0) {
+      const reader = new FileReader();
+      this.imagePath = event.target.files;
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = () => { // called once readAsDataURL is completed
+        this.url = reader.result; // add source to image
+      };
+    }
+  }
+
+  onStart(elementRef: ElementRef) {
+    const apiUrl = `${environment.apiUrl}/api/image/resize`;
+    const observer = this.http.post(apiUrl, { image: elementRef, options: { width: 100, height: 100 } });
+    observer.subscribe({
+      next: (res) => {
+        console.log(res);
+      }
+    });
+
+  }
 }
